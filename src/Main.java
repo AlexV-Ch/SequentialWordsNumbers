afd9c6b12c2f9d7d30198788a2a1dd42f7d90e19
auto_1 <alexch@yandex.ru>
2025-01-31 11:09:22 +0300

    9.9 Практическая работа (автотесты) Задание 1 Исправлено

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,29 @@ public class Main {
     while (true) {
         System.out.println("Введите текст: ");
         String text =new Scanner(System.in).nextLine();
-        System.out.println(newLine(text));
+        System.out.println(sequentialWordsNumbers(text));
     }
     }
-    public static String newLine(String text) {
+    public static String sequentialWordsNumbers(String text) {
         text = text.trim();
         int i = 0;
         int begin = 0;
         int end = 0;
-        String newtext = "";
+        //String newtext = "";
+        StringBuilder newtext = new StringBuilder();
         if (text.length() != 0) {
             while (text.indexOf(' ', begin) > -1) {
                 end = text.indexOf(' ', begin);
-                newtext += "(" + ++i + ") " + text.substring(begin, end) + " ";
+                newtext.append("(" + ++i + ") " + text.substring(begin, end) + " ");
+
                 begin = end + 1;
             }
-            newtext += "(" + ++i + ") " + text.substring(begin) + " ";
+            newtext.append("(");
+            newtext.append(++i);
+            newtext.append(") ");
+            newtext.append(text.substring(begin));
+            newtext.append(" ");
         }
-            return newtext;
+            return newtext.toString();
         }
 }
