4cdf6758955335ce5e56bb24e98bd51d318139d7
AlexV-Ch <alexch@yandex.ru>
2025-02-02 18:01:33 +0300

    9.9 Практическая работа (автотесты) Задание 3 готово! + проверка

--- src/PhoneCleanerRegex.java
+++ src/PhoneCleanerRegex.java
@@ -1,20 +1,42 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PhoneCleanerRegex {
     public static String checkingNumber(String number) {
-        String regex = "[^0-9]";
-        number = number.replaceAll(regex, "");
-        int firstNumber = Integer.parseInt(number.substring(0, 1));
+        number = number.replaceAll("[^0-9]", "");
 
-        if(number.length() == 11 && (firstNumber == 7 || firstNumber == 8)) {
-            number = "7" + number.substring(1) + "- Номер верный";
+        if (number.length() == 11) {
+            if (number.charAt(0) == '8') {
+                number = "7" + number.substring(1);
+            } else if (number.charAt(0) != '7') {
+                return "Неверный формат номера";
+            }
         } else if (number.length() == 10) {
-            number = "7" + number + "- Номер верный";
-        } else if (number.length() > 11 || number.length() < 10) {
-            number = " Неверный формат номера ";
-        } else if (number.length() == 11 && (firstNumber != 7 || firstNumber != 8)) {
-            number = " Неверный формат номера ";
+            number = "7" + number;
         } else {
-            number = " Что то не так";
+            return "Неверный формат номера";
         }
-        return number;
+        Pattern pattern = Pattern.compile("^7\\d{10}$");
+        Matcher matcher = pattern.matcher(number);
+        if (matcher.matches()) {
+            return number;
+        } else {
+            return "Неверный формат номера";
+        }
+
+
+
+//        if(number.length() == 11 && (firstNumber == 7 || firstNumber == 8)) {
+//            number = "7" + number.substring(1) + "- Номер верный";
+//        } else if (number.length() == 10) {
+//            number = "7" + number + "- Номер верный";
+//        } else if (number.length() > 11 || number.length() < 10) {
+//            number = " Неверный формат номера ";
+//        } else if (number.length() == 11 && (firstNumber != 7 || firstNumber != 8)) {
+//            number = " Неверный формат номера ";
+//        } else {
+//            number = " Что то не так";
+//        }
+//        return number;
     }
 }
